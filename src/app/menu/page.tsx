7edf0cb90ac1ca911299fa5cd5f4b404f200633e
Raymond Dela Cruz <echopogi2525.edc@gmail.com>
2024-01-22 22:48:59 +0800

    Update page.tsx

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -13,9 +13,15 @@ const MenuPage = () => {
           style={{ backgroundImage: `url(${category.img})` }}
         >
           <div className={`text-${category.color} w-1/2`}>
-            <h1 className="uppercase font-bold text-2xl md:text-3xl">{category.title}</h1>
+            <h1 className="uppercase font-bold text-3xl">{category.title}</h1>
             <p className="text-sm my-2 md:my-8">{category.desc}</p>
-            <button className={`hidden 2xl:block bg-${category.color} text-${category.color === "black" ? "white" : "red-500"} py-2 px-4 rounded-md`}>Explore</button>
+            <button
+              className={`hidden 2xl:block bg-${category.color} text-${
+                category.color === "black" ? "white" : "red-500"
+              } py-2 px-4 rounded-md`}
+            >
+              Explore
+            </button>
           </div>
         </Link>
       ))}
